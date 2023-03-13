117c8ee60d8c74c1474d13691abab185695d24b2
eliepartouche1 <126602122+eliepartouche1@users.noreply.github.com>
2023-03-13 20:24:55 +0200

    Create assignmentTask.java

---
+++ src/lesson_3/assignmentTask.java
@@ -0,0 +1,21 @@
+package lesson_3;
+
+public class assignmentTask {
+    public static void main(String[] args) {
+
+        var a = 15;
+        a = 10;
+        a += 90;
+        a -= 25;
+        a /= 3;
+        a %= 10;
+        a *= (-3);
+        ++a;
+        --a;
+        System.out.println("a = " + a);
+
+
+
+
+    }
+}
