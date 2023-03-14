294c771ab5f4992b66ff2fe2d4a7410a4045b262
eliepartouche1 <126602122+eliepartouche1@users.noreply.github.com>
2023-03-14 11:52:42 +0200

    hommetask

---
+++ src/lesson_3/HomeTasks/CheckNumbers.java
@@ -0,0 +1,35 @@
+package lesson_3.HomeTasks;
+
+public class CheckNumbers {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 22;
+        int c = 55;
+        int d = 25;
+        int e = 10;
+
+        System.out.println (a%2 == 0);
+        System.out.println(a%7 == 1);
+        System.out.println(b%2 == 0);
+        System.out.println(b%7 == 1);
+        System.out.println(c%2 == 0);
+        System.out.println(c%7 == 1);
+        System.out.println(d%2 == 0);
+        System.out.println(d%7 == 1);
+        System.out.println(e%2 == 0);
+        System.out.println(e%7 == 1);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
