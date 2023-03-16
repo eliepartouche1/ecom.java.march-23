90bf5e4163191683d4e7c94c6dc81172e47980c8
eliepartouche1 <126602122+eliepartouche1@users.noreply.github.com>
2023-03-16 13:28:49 +0200

    Update compressionTask.java

--- src/lesson_3/HomeTasks/compressionTask.java
+++ src/lesson_3/HomeTasks/compressionTask.java
@@ -2,25 +2,21 @@ package lesson_3.HomeTasks;
 
 public class compressionTask {
     public static void main(String[] args) {
-         System.out.println(10>6);
+        System.out.println(10>6);
         System.out.println(5>=5);
         System.out.println(20<10);
-        System.out.println(35 -= 50);
+        System.out.println(35<= 50);
         System.out.println(25 == 25);
         System.out.println(50 != 50);
 
         String a = new String("city");
         System.out.println(a = "city");
 
-        String b =("city");
-        System.out.println(b = "city");
-
-        System.out.println(a==b);
 
         String c = new String("citY");
         System.out.println(c);
 
-        System.out.println(a.equals(b));
-        System.out.println(b.equals(c));
+        System.out.println(a.equalsIgnoreCase(c));
+
     }
 }
