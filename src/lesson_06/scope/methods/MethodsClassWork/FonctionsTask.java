53483ca07fce3d5c4c650f13707ea678e4a2c9c8
eliepartouche1 <126602122+eliepartouche1@users.noreply.github.com>
2023-03-23 20:00:12 +0200

    Update

--- src/lesson_06/scope/methods/MethodsClassWork/FonctionsTask.java
+++ src/lesson_06/scope/methods/MethodsClassWork/FonctionsTask.java
@@ -5,17 +5,39 @@ import java.util.ArrayList;
 public class FonctionsTask {
     public static void main(String[] args) {
 
-        ArrayList<Integer>numbres = new ArrayList<>();
-        System.out.println((int)( Math.random()* 1000));
+        ArrayList<Integer> numbers = new ArrayList<>();
 
 
-         for (int i = 0;i<10; i++) {
-        numbres.add((int)(Math.random()* 1000));
+        for (int i = 0; i < 10; i++) {
+            numbers.add((int) (Math.random() * 1000));
+        }
+
+        printArrayList(numbers);
+        System.out.println("\ngetMaxNumberOfArrayList(numbers) = " + getMaxNumberOfArrayList(numbers));
+
     }
-    }
-public static void prinArrayList (ArrayList<Integer>arrayList){
 
-}
+    public static void printArrayList(ArrayList<Integer> arrayList) {
+
+        arrayList.forEach((number) -> System.out.print(number + " "));
+    }
+
+    public static int getMaxNumberOfArrayList(ArrayList<Integer> arrayList) {
+
+        int maxNumber = arrayList.get(0); // 5
+        // maxNumber = 5  index 1 = 7
+
+        for (int i = 1; i < arrayList.size(); i++){
+            if (maxNumber > arrayList.get(i)) {
+                maxNumber = maxNumber;
+            }
+            else {
+                maxNumber = arrayList.get(i);
+            }
+        }
+        System.out.println("maxNumber = " + maxNumber);
+        return  maxNumber;
+    }
 
 
 
